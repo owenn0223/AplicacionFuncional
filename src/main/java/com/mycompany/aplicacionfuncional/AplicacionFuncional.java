3cc90235e8a8a9d662e3027ca6ef2ffef029e8d9
owen fernando <bareno.owen.9508@eam.edu.co>
2025-02-21 11:16:28 -0500

    primer commit

---
+++ src/main/java/com/mycompany/aplicacionfuncional/AplicacionFuncional.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aplicacionfuncional;
+
+/**
+ *
+ * @author PC
+ */
+public class AplicacionFuncional {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
