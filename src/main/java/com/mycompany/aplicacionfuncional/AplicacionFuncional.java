4c5658185a5f4d75bbefaf244de261ca2513767b
owen fernando <bareno.owen.9508@eam.edu.co>
2025-02-21 11:22:23 -0500

    bienvenidos

--- src/main/java/com/mycompany/aplicacionfuncional/AplicacionFuncional.java
+++ src/main/java/com/mycompany/aplicacionfuncional/AplicacionFuncional.java
@@ -11,6 +11,6 @@ package com.mycompany.aplicacionfuncional;
 public class AplicacionFuncional {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("esto es una aplicacion funcional");
     }
 }
